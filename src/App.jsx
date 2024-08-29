fbe54ba82d321b1c36bb3bbb5ae2a20dca4a3d5f
Farhan <farhancc123@gmail.com>
2024-08-29 09:34:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/Order/new", element: <CreateOrder /> },
+  { path: "/Order/:orderId", element: <Order /> },
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
